f8054641192dad6e67fde700c74de74e61ed6bfa
Dhiraj Patil <ardhirajrp@gmail.com>
2022-09-17 03:34:03 +0530

    220917_FinalFile

---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    public float SpinForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Rotate (0, SpinForce * Time.deltaTime, 0); 
+
+    }
+
+    public void ChangeSpin()
+    {
+        SpinForce = -SpinForce;
+    }
+}
